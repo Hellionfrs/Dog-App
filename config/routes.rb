b9082032e27f1d766cffd010002967e70196e2f8
“Hellionfrs” <fredy.rodriguez@pucp.pe>
2022-08-01 23:32:29 -0500

    home and about controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  get 'about', to: 'pages#about'
 
+  root "pages#home"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
