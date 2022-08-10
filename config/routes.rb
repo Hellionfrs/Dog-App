a70af3a99b2003b7d5badf1c44ec65a2923c7602
“Hellionfrs” <fredy.rodriguez@pucp.pe>
2022-08-09 23:30:39 -0500

    add Comments at Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/profile'
   devise_for :users,  controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations' 
   }
+  get '/u/:id', to: 'users#profile', as: 'user'
 
-  resources :posts
+  # /posts/1/comments/4
+  resources :posts  do
+    resources  :comments
+  end
   
   get 'about', to: 'pages#about'
 
